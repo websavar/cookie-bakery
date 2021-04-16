d915b2e38b6866cdbb51f287becd24a6fc90279a
= <websavar@gmail.com>
2021-04-16 10:58:08 +0200

    add roof component

---
+++ src/components/House/Roof/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss'
+
+const Roof = () => {
+    return <div className="roof"></div>
+}
+
+export default Roof;
--- src/components/House/index.js
+++ src/components/House/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import './index.scss';
+import Roof from './Roof';
 
 const House = () => {
 
     return (
         <div className="d-flex flex-column col-12 m-auto mb-5">
+            <div className="m-auto">
+                <Roof />
+            </div>
             <div className="body m-auto">
             </div>
         </div>
